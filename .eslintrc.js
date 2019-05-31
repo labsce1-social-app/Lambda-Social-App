847bbe64bc033f3cefc85851f358b5a14b793f2f
Carlos Lantigua <clantigua954@gmail.com>
2019-05-31 12:38:00 -0400

    initializing server files

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    'jest/globals': true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'jest', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1
+      }
+    ],
+    'no-console': 1,
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'none',
+        ignoreRestSiblings: true
+      }
+    ]
+  }
+}
