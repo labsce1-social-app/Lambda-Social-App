c7a35a40d1523f6e420bdd71194867250ad6734a
mparedes003 <onnabugeishadev@gmail.com>
2019-05-31 15:32:40 -0700

    Add seeds in comment table

---
+++ server/data/seeds/005-Comments.js
@@ -0,0 +1,11 @@
+const { genComments } = require('../utils/');
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('comment')
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex('comment').insert(genComments);
+    });
+};
