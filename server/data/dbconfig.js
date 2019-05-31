480b8cc843eff8d48e5895ce03b8592530988cc7
TRIF3X <thewizardofcrypto@gmail.com>
2019-05-31 16:49:28 -0500

    set up middleware, prettier all files

--- server/data/dbconfig.js
+++ server/data/dbconfig.js
@@ -2,4 +2,4 @@ const knex = require('knex');
 const knexconfig = require('../knexfile.js');
 const enviornment = process.env.NODE_ENV || 'development';
 
-module.exports = knex(knexconfig[enviornment])
+module.exports = knex(knexconfig[enviornment]);
