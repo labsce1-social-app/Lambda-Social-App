c2a25c29678c86dde23ec2ee344f3fb0dc7372d5
Carlos Lantigua <clantigua954@gmail.com>
2019-05-31 17:00:29 -0400

    created 3 tables that will be used for users, subtopics and joining later

---
+++ server/data/migrations/20190531153256_Subtopics.js
@@ -0,0 +1,26 @@
+
+
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('subtopic', subtopic => {
+        subtopic
+            .increments('id');
+
+        subtopic
+            .string('title', 50)
+            .unique()
+            .notNullable()
+
+        subtopic
+            .integer('creater_id')
+            .references('id')
+            .inTable('user')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+    })
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('subtopic');
+};
+
+// SELECT 'team' WHERE 'id' = 'subtopic_id'
---
+++ server/data/seeds/003-Subtopic_users.js
@@ -0,0 +1,10 @@
+const { genSubtopicUsers } = require('../utils/')
+
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('subtopic_users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('subtopic_users').insert(genSubtopicUsers);
+    });
+};
