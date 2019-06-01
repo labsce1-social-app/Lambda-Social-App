c9716db8e878ace96c51ff9406d619cc89c11135
TRIF3X <thewizardofcrypto@gmail.com>
2019-05-31 21:09:42 -0500

    deploy to heroku and add instruction to index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+// install heroku global `npm install -g heroku`
+// login heroku `heroku login`
+//From the top level of repo use command `git subtree push --prefix server heroku-backend master`
+
 const server = require('./api/server.js');
 const port = process.env.PORT || 3000;
 
