c2a702c0275ef88839a2ba56d02d9f0d6bd0f85e
Carlos Lantigua <clantigua954@gmail.com>
2019-06-02 22:39:28 -0400

    got rid of useless imports and cleaned up the code a bit

--- react-native-app/src/common/SearchBar.js
+++ react-native-app/src/common/SearchBar.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { Container, Header, Item, Input, Icon, Button, Text } from 'native-base';
+import { Header, Item, Input, Icon, Button, Text } from 'native-base';
 
 const SearchBar = (props) => {
     return (
-        <>
-            <Header searchBar rounded>
-                <Item>
-                    <Icon name="ios-search" />
-                    <Input placeholder="Search" />
-                    <Icon name="ios-people" />
-                </Item>
-                <Button transparent>
-                    <Text>Search</Text>
-                </Button>
-            </Header>
-        </>
+        <Header searchBar rounded>
+            <Item>
+                <Icon name="ios-search" />
+                <Input placeholder="Search" />
+                <Icon name="ios-people" />
+            </Item>
+            <Button transparent>
+                <Text>Search</Text>
+            </Button>
+        </Header>
     );
 };
 
