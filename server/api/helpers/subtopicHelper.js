022ea825aba4de9c70877b2219ec3e7cf5769809
TRIF3X <thewizardofcrypto@gmail.com>
2019-06-04 13:26:22 -0500

    subtopic router, check for valid user

---
+++ server/api/helpers/subtopicHelper.js
@@ -0,0 +1,23 @@
+const db = require('../../data/dbconfig.js');
+
+// checks to see if creater_id is a valid user id
+const checkValidUser = async creater_id => {
+  let isValid = false;
+
+  await db('user')
+    .where('id', creater_id)
+    .then(id => {
+      if (id.length > 0) {
+        isValid = true;
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  return isValid;
+};
+
+module.exports = {
+  checkValidUser
+};
--- server/api/routers/subtopicRouter.js
+++ server/api/routers/subtopicRouter.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const db = require('../../data/dbconfig.js');
+const { checkValidUser } = require('../helpers/subtopicHelper.js');
 
 /*
 GET ROUTE get all subtopics
@@ -36,6 +37,36 @@ router.get('/subtopics/:id', (req, res) => {
     .catch(err => {
       res.status(500).json({ error: err });
     });
+});
+
+/*
+POST ROUTE create a subtopic
+@BODY = {
+    title: !STRING >= 50 characters
+    creater_id: !INT
+}
+ROUTE = '/api/subtopics/create
+returns = [single subtopic]
+*/
+
+router.post('/subtopics/create', async (req, res) => {
+  const body = req.body;
+
+  if (
+    body.title.length === 0 ||
+    body.title.length > 50 ||
+    body.title === '' ||
+    body.creater_id === null
+  ) {
+    res.status(400).json({
+      message:
+        'title must be between 0 and 50 charecters,creater_id must be valid'
+    });
+  } else if ((await checkValidUser(body.creater_id)) === false) {
+    res.status(500).json({ error: 'no valid user found' });
+  } else {
+    res.status(200).send('ok');
+  }
 });
 
 module.exports = router;
