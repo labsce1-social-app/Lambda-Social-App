7b1eac41d2d986ea177db02edcb4e9231320f164
Michael Agard <mdagard@gmail.com>
2019-06-04 17:35:42 -0400

    add stylesheet for PostSummary.js

---
+++ react-native-app/components/postsummary/Style.js
@@ -0,0 +1,57 @@
+import { StyleSheet } from 'react-native';
+export default StyleSheet.create({
+  // Post Summary Container
+  viewContainer: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  // Post Summary Text Container for vertical stacking
+  textView: {
+    flexDirection: 'column',
+    width: `80%`,
+    marginLeft: 5
+  },
+  // Contains
+  line1: {
+    flexDirection: 'row'
+  },
+  line2: {
+    flexDirection: 'row'
+  },
+  line3: {
+    flexDirection: 'row'
+  },
+  // Post Summary Icon
+  icon: {
+    width: 48,
+    height: 48
+  },
+  // Post Summary Title
+  title: {
+
+  },
+  // Post Summary SubTopic Origin
+  origin: {
+
+  },
+  // Post Summary Username
+  name: {
+
+  },
+  // Post Summary Date
+  date: {
+
+  },
+  // Post Summary Truncated Text Comment
+  comment: {
+
+  },
+  // Post Summary Comment Count
+  commentCount: {
+
+  },
+  // Post Summary Upvotes
+  upvotes: {
+
+  }
+});
