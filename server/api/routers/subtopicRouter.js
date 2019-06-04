27a3e9b5f6880fd0e5a6e7e8134e7f745bfa79ce
TRIF3X <thewizardofcrypto@gmail.com>
2019-06-04 12:39:54 -0500

    setup subtopic router file

---
+++ server/api/routers/subtopicRouter.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const db = require('../../data/dbconfig.js');
+
+/*
+GET ROUTE get all subtopics
+@PARAM = NONE
+ROUTE = '/api/subtopics
+returns = [all subtopics]
+*/
+
+/*
+GET ROUTE get single subtopic
+@PARAM = ID
+ROUTE = '/api/subtopics/:id
+returns = [single subtopic]
+*/
+
+module.exports = router;
