50cd69204bacfd6d5131f05da84d91eff8e5b71c
Michael Agard <mdagard@gmail.com>
2019-06-04 17:52:49 -0400

    add propTypes and defaultProps

--- react-native-app/components/postsummary/PostSummary.js
+++ react-native-app/components/postsummary/PostSummary.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Text, View, Image } from 'react-native';
 import style from './Style';
+import PropTypes from 'prop-types';
+import ErrorIcon from './_ionicons_svg_md-alert.svg';
+
 
 const PostSummary = props => {
     return (<View style={style.viewContainer}>
-        <Image style={style.icon} source={props.icon} />
+        <Image style={style.icon} source={ErrorIcon} />
         <View style={style.textView}>
           <View style={style.line1}>
             <Text style={style.title}>{props.title}</Text>
@@ -22,6 +25,24 @@ const PostSummary = props => {
           </View>
         </View>
       </View>)
+}
+
+PostSummary.defaultProps = {
+  icon: ErrorIcon,
+  title: 'No Title Provided',
+  subTopic: 'No SubTopic Provided',
+  name: 'No Name Provided',
+  date: 'No Date Provided',
+  comment: 'No Comment Provided'
+};
+
+PostSummary.propTypes = {
+  icon: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  subTopic: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  comment: PropTypes.string.isRequired
 }
 
 export default PostSummary;
