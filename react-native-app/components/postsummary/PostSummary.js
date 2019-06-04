c46979f56c0c360af9bc7fda54c9db2ca6f74776
Michael Agard <mdagard@gmail.com>
2019-06-04 17:37:38 -0400

    remove hardcoded image and add props for image

--- react-native-app/components/postsummary/PostSummary.js
+++ react-native-app/components/postsummary/PostSummary.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { Text, View, Image } from 'react-native';
-import { Container, Button } from 'native-base';
 import style from './Style';
-
-// Set up dummy image then redux method to get user image for post.
-let icon = require('./my-icon.png');
 
 const PostSummary = props => {
     return (<View style={style.viewContainer}>
-        <Image style={style.icon} source={icon} />
+        <Image style={style.icon} source={props.icon} />
         <View style={style.textView}>
           <View style={style.line1}>
             <Text style={style.title}>{props.title}</Text>
