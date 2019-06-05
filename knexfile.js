97875b3edcdb956734e120534ff0bcc2e0837bc6
Clantigua2 <clantigua954@gmail.com>
2019-06-05 19:21:16 -0400

    changing pg dir

--- knexfile.js
+++ knexfile.js
@@ -32,17 +32,13 @@ module.exports = {
 
   production: {
     client: 'pg',
+    // The next line is where the application will read that environment variable to connect to the database
     connection: process.env.DATABASE_URL,
-    useNullAsDefault: true,
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
-      directory: './server/data/migrations'
+      directory: __dirname + '/db/migrations',
     },
     seeds: {
-      directory: './server/data/seeds'
-    }
-  }
+      directory: __dirname + '/db/seeds/production',
+    },
+  },
 };
