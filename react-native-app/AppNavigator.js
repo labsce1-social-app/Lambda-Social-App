3a8ae7c0ce6f31d99073cda8f77af0a950b10691
Luisdh <luisd824@gmail.com>
2019-06-05 01:26:53 -0400

    added bottom tab nav initial styles

--- react-native-app/AppNavigator.js
+++ react-native-app/AppNavigator.js
@@ -1,26 +1,46 @@
 import {
   createDrawerNavigator,
   createAppContainer,
-  createStackNavigator,
-  createBottomTabNavigator
+  createStackNavigator
 } from 'react-navigation';
+import { createBottomTabNavigator, BottomTabBar } from 'react-navigation-tabs';
+import { Icon } from 'native-base';
+
+import React from 'react';
 
 // import ComponentName from './.'
 import HomePage from './src/views/LandingPage';
-
 import Login from './auth0/index';
 
 /** default stack navigation **
  * Drawer and tab Navigator need custom components
  */
-const AppNavigator = createStackNavigator(
+
+const AppNavigator = createBottomTabNavigator(
   {
     // RouteName: ComponentName,
-    Home: HomePage,
-    Login: Login
+    Home: {
+      screen: HomePage
+    },
+    Posts: {
+      screen: Login, // name needs to change
+      navigationOptions: {
+        tabBarIcon: <Icon name="paper-plane" />
+      }
+    }
   },
   {
-    initialRouteName: 'Home'
+    tabBarOptions: {
+      activeTintColor: '#990000',
+      inactiveTintColor: '#586589',
+      style: {
+        // color: '#FFFFFF'
+        // backgroundColor: '#171F33'
+      }
+    }
+  },
+  {
+    initialRouteName: 'Login'
   }
 );
 
