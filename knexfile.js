9b22d7e828a85bc13cfb5dff0f4e31bad59b3469
Clantigua2 <clantigua954@gmail.com>
2019-06-05 15:26:52 -0400

    trying to fix heroku issues

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: `${process.env.DATABASE_URL}`,
+    connection: process.env.DATABASE_URL,
     ssl: true,
     migrations: {
       tableName: 'knex_migrations',
