b110e779cbd63516345dd0753c41a9fa47c68a97
Clantigua2 <clantigua954@gmail.com>
2019-06-05 18:18:42 -0400

    last commit didn't work, it broke it even worst

--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,30 @@
 require('dotenv').config();
 
 module.exports = {
-  // development: {
-  client: 'sqlite3',
-  connection: {
-    filename: './server/data/dev.sqlite3'
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './server/data/dev.sqlite3'
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './server/data/migrations'
+    },
+    seeds: {
+      directory: './server/data/seeds'
+    },
+    useNullAsDefault: true
   },
-  migrations: {
-    tableName: 'knex_migrations',
-    directory: './server/data/migrations'
-  },
-  seeds: {
-    directory: './server/data/seeds'
-  },
-  useNullAsDefault: true
-  // },
-  // production: {
-  //   client: 'pg',
-  //   connection: process.env.DATABASE_URL,
-  //   ssl: true,
-  //   migrations: {
-  //     tableName: 'knex_migrations',
-  //     directory: './server/data/migrations'
-  //   },
-  //   seeds: {
-  //     directory: './server/data/seeds'
-  //   }
-  // }
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    ssl: true,
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './server/data/migrations'
+    },
+    seeds: {
+      directory: './server/data/seeds'
+    }
+  }
 };
