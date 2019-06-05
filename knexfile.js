e8d5458befcee403b73e05b76c89c7e29008682b
Clantigua2 <clantigua954@gmail.com>
2019-06-05 19:26:05 -0400

    messing with pg still

--- knexfile.js
+++ knexfile.js
@@ -14,31 +14,34 @@ module.exports = {
     useNullAsDefault: true
   },
 
-  staging: {
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user: 'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'pg',
-    // The next line is where the application will read that environment variable to connect to the database
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: __dirname + '/db/migrations',
+      directory: './server/data/migrations'
     },
     seeds: {
-      directory: __dirname + '/db/seeds/production',
-    },
-  },
+      directory: './server/data/seeds'
+    }
+  }
 };
