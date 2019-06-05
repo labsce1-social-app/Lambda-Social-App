e942e860a44a60e82a5eea1ab2d2be295340211d
Clantigua2 <clantigua954@gmail.com>
2019-06-05 18:48:40 -0400

    trying to go full sqlite3

--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,30 @@
-require('dotenv').config();
-
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './server/data/dev.sqlite3'
+      filename: './data/dev.sqlite3'
     },
     migrations: {
       tableName: 'knex_migrations',
-      directory: './server/data/migrations'
+      directory: './data/migrations'
     },
     seeds: {
-      directory: './server/data/seeds'
+      directory: './data/seeds'
     },
     useNullAsDefault: true
   },
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    ssl: true,
+    client: 'sqlite3',
+    connection: {
+      filename: './data/dev.sqlite3'
+    },
     migrations: {
       tableName: 'knex_migrations',
-      directory: './server/data/migrations'
+      directory: './data/migrations'
     },
     seeds: {
-      directory: './server/data/seeds'
-    }
+      directory: './data/seeds'
+    },
+    useNullAsDefault: true
   }
 };
