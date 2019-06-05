50999bea404e374dab06d130a095b8e67e812b03
Clantigua2 <clantigua954@gmail.com>
2019-06-05 12:20:39 -0400

    moved main server files to root

--- server/data/dbconfig.js
+++ server/data/dbconfig.js
@@ -1,5 +1,5 @@
 const knex = require('knex');
-const knexconfig = require('../knexfile.js');
+const knexconfig = require('../../knexfile.js');
 const enviornment = process.env.NODE_ENV || 'development';
 
 module.exports = knex(knexconfig[enviornment]);
