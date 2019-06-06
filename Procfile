c0f7c15a1f6cc2a947aeb64f113b7bc8ec659dc8
Clantigua2 <clantigua954@gmail.com>
2019-06-05 21:44:38 -0400

    fixed procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: knex migrate:rollback && knex migrate:latest && knex seed: run && node index.js
+web: knex migrate:rollback && knex migrate:latest && knex seed:run && node index.js
--- knexfile.js
+++ knexfile.js
@@ -13,30 +13,9 @@ module.exports = {
     },
     useNullAsDefault: true
   },
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user: 'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
       directory: './server/data/migrations'
     },
