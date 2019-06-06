c5f7daf9d2c72e043078acd8736ba87a5ac7ad37
Luisdh <luisd824@gmail.com>
2019-06-06 15:47:06 -0400

    Fix fontloading error by adding loading screen

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-// custom font loader for native base
-import { Font } from 'expo';
-import { Ionicons } from '@expo/vector-icons';
-import { Container, Content } from 'native-base';
 
+// custom font loader for native base
+import { AppLoading } from 'expo';
 import AppNavigator from './AppNavigator';
+import * as Font from 'expo-font';
 
 // memory usage optimize for screen changing
 import { useScreens } from 'react-native-screens';
-useScreens();
+import Wrapper from './src/components/Wrapper';
+import NativeHeader from './src/common/NativeHeader';
+// useScreens();
 
 export default class App extends React.Component {
-  async componentDidMount() {
+  state = {
+    loading: false
+  };
+
+  async componentWillMount() {
     // mount custom fonts for native base components
-    await Font.loadAsync({
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-      ...Ionicons.font
-    });
+    try {
+      await Font.loadAsync({
+        Roboto: require('native-base/Fonts/Roboto.ttf'),
+        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf')
+      });
+
+      this.setState({ loading: true });
+    } catch (error) {
+      console.log('error loading fonts: ', error);
+    }
   }
+
   render() {
-    return <AppNavigator />;
+    const { loading } = this.state;
+    if (!loading) {
+      return <AppLoading />;
+    }
+    return (
+      <Wrapper>
+        <NativeHeader />
+        <AppNavigator />
+      </Wrapper>
+    );
   }
 }
