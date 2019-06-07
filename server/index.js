b39fa4d830ffaedb8d0948540074427effe471e8
TRIF3X <thewizardofcrypto@gmail.com>
2019-06-07 08:09:46 -0500

    test

---
+++ server/index.js
@@ -0,0 +1,11 @@
+// install heroku global `npm install -g heroku`
+// login heroku `heroku login`
+//From the top level of repo use command `git subtree push --prefix server heroku-backend master`
+
+const server = require('./api/server.js');
+const port = process.env.PORT || 3000;
+
+// start the server
+server.listen(port, () => {
+  console.log(`\n=== Server listening on port ${port} ===\n`);
+});
