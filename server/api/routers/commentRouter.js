65813e799231d1c08e805c0048cd16b0867a5348
TRIF3X <thewizardofcrypto@gmail.com>
2019-06-13 11:23:53 -0500

    update comment by discussion_id route

--- server/api/routers/commentRouter.js
+++ server/api/routers/commentRouter.js
@@ -3,18 +3,18 @@ const db = require('../../data/dbconfig.js');
 const { getCommentsByDiscussionId } = require('../helpers/index.js');
 
 router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    getCommentsByDiscussionId(id)
-        .then((comments) => {
-            if (comments) {
-                res.status(200).json(comments);
-            } else {
-                res.status(404).json({ message: 'no comments yet' })
-            }
-        })
-        .catch((error) => {
-            res.status(500).json({ error: 'Server error', error })
-        })
-})
+  const { id } = req.params;
+  getCommentsByDiscussionId(id)
+    .then(comments => {
+      if (comments) {
+        res.status(200).json(comments);
+      } else {
+        res.status(404).json({ message: 'no comments yet' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Server error', error });
+    });
+});
 
 module.exports = router;
