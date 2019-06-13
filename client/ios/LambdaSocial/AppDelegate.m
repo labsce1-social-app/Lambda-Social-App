095232546d1e65d6d15461a8fba9ecdbbea35126
Clantigua2 <clantigua954@gmail.com>
2019-06-13 15:09:32 -0400

    renames application to Lambda Social, previously it was just Client which is stupid

--- client/ios/LambdaSocial/AppDelegate.m
+++ client/ios/LambdaSocial/AppDelegate.m
@@ -25,7 +25,7 @@
 {
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"client"
+                                                   moduleName:@"LambdaSocial"
                                             initialProperties:nil];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
