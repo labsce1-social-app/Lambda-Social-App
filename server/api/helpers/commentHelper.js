53f9bd0947c63caa865e475823caed0e6cddf23e
TRIF3X <thewizardofcrypto@gmail.com>
2019-06-13 13:22:17 -0500

    get comment by id, update discussion router checks

--- server/api/helpers/commentHelper.js
+++ server/api/helpers/commentHelper.js
@@ -35,7 +35,26 @@ const getCommentsAndJoinUser = () => {
     `);
 };
 
+const getCommentsAndJoinUserById = id => {
+  return db.raw(`
+      SELECT
+      user.username,
+      comment.id,
+      comment.comment_post,
+      comment.user_id, 
+      comment.created_at, 
+      comment.updated_at, 
+      comment.discussion_id,
+      comment.comment_id
+      FROM comment
+      JOIN user
+      WHERE comment.user_id = user.id
+      AND comment.id = ${id}
+      `);
+};
+
 module.exports = {
   getCommentsByDiscussionId,
-  getCommentsAndJoinUser
+  getCommentsAndJoinUser,
+  getCommentsAndJoinUserById
 };
--- server/api/routers/commentRouter.js
+++ server/api/routers/commentRouter.js
@@ -2,12 +2,16 @@ const router = require('express').Router();
 const db = require('../../data/dbconfig.js');
 const {
   getCommentsByDiscussionId,
-  getCommentsAndJoinUser
+  getCommentsAndJoinUser,
+  getCommentsAndJoinUserById
 } = require('../helpers/index.js');
+
+// used for updated timestamps
+const moment = require('moment');
+let timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
 
 /*
 GET ROUTE get all comments
-@PARAM = ID
 ROUTE = '/comments
 returns = array of all comments
 TESTS: {
@@ -27,7 +31,9 @@ router.get('/', (req, res) => {
 
 /*
 GET ROUTE get comments by discussion_id
-@PARAM = ID
+@PARAM = {
+    id: !INT
+}
 ROUTE = '/comments/d/:id
 returns = array of comments by discussion_id
 TESTS: {
@@ -39,7 +45,7 @@ router.get('/d/:id', (req, res) => {
   const { id } = req.params;
   getCommentsByDiscussionId(id)
     .then(comments => {
-      if (comments) {
+      if (comments.length > 0) {
         res.status(200).json(comments);
       } else {
         res.status(404).json({ message: 'no comments yet' });
@@ -47,6 +53,34 @@ router.get('/d/:id', (req, res) => {
     })
     .catch(error => {
       res.status(500).json({ error: 'Server error', error });
+    });
+});
+
+/*
+GET ROUTE get comment by id
+@PARAM = {
+    id: !INT
+}
+ROUTE = '/comments/:id
+returns = a single comment by id
+TESTS: {
+    1) RETURNS A SINGLE COMMENT BY ID 
+}
+*/
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  getCommentsAndJoinUserById(id)
+    .then(comment => {
+      if (comment.length > 0) {
+        res.status(200).json(comment);
+      } else {
+        res.status(404).json({ message: 'comment not found' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'server error', err });
     });
 });
 
