b12501c929c2f51c22ce779fff39c71a7f877b99
TRIF3X <thewizardofcrypto@gmail.com>
2019-06-13 13:03:19 -0500

    get all comments

--- server/api/helpers/commentHelper.js
+++ server/api/helpers/commentHelper.js
@@ -1,23 +1,41 @@
 const db = require('../../data/dbconfig.js');
 
-const getCommentsByDiscussionId = async discussion_id => {
+const getCommentsByDiscussionId = discussion_id => {
   return db.raw(`
-  SELECT
-  user.username,
-  comment.id,
-  comment.comment_post,
-  comment.user_id, 
-  comment.created_at, 
-  comment.updated_at, 
-  comment.discussion_id,
-  comment.comment_id
-  FROM comment
-  JOIN user
-  WHERE comment.user_id = user.id
-  AND comment.discussion_id = ${discussion_id}
- `);
+    SELECT
+    user.username,
+    comment.id,
+    comment.comment_post,
+    comment.user_id, 
+    comment.created_at, 
+    comment.updated_at, 
+    comment.discussion_id,
+    comment.comment_id
+    FROM comment
+    JOIN user
+    WHERE comment.user_id = user.id
+    AND comment.discussion_id = ${discussion_id}
+    `);
+};
+
+const getCommentsAndJoinUser = () => {
+  return db.raw(`
+    SELECT
+    user.username,
+    comment.id,
+    comment.comment_post,
+    comment.user_id, 
+    comment.created_at, 
+    comment.updated_at, 
+    comment.discussion_id,
+    comment.comment_id
+    FROM comment
+    JOIN user
+    WHERE comment.user_id = user.id
+    `);
 };
 
 module.exports = {
-  getCommentsByDiscussionId
+  getCommentsByDiscussionId,
+  getCommentsAndJoinUser
 };
--- server/api/routers/commentRouter.js
+++ server/api/routers/commentRouter.js
@@ -1,8 +1,41 @@
 const router = require('express').Router();
 const db = require('../../data/dbconfig.js');
-const { getCommentsByDiscussionId } = require('../helpers/index.js');
+const {
+  getCommentsByDiscussionId,
+  getCommentsAndJoinUser
+} = require('../helpers/index.js');
 
-router.get('/:id', (req, res) => {
+/*
+GET ROUTE get all comments
+@PARAM = ID
+ROUTE = '/comments
+returns = array of all comments
+TESTS: {
+    1) RETURNS ALL COMMENTS 
+}
+*/
+
+router.get('/', (req, res) => {
+  getCommentsAndJoinUser()
+    .then(comments => {
+      res.status(200).json(comments);
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'server error', err });
+    });
+});
+
+/*
+GET ROUTE get comments by discussion_id
+@PARAM = ID
+ROUTE = '/comments/d/:id
+returns = array of comments by discussion_id
+TESTS: {
+    1) RETURNS ALL COMMENTS BY DISCUSSION_ID
+}
+*/
+
+router.get('/d/:id', (req, res) => {
   const { id } = req.params;
   getCommentsByDiscussionId(id)
     .then(comments => {
