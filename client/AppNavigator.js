c6c655f9b3cc509047a6653cb631a918ddb0ab9f
mparedes003 <onnabugeishadev@gmail.com>
2019-06-15 12:50:30 -0700

    Setup PrivateRoutes in prep for login restriction

--- client/AppNavigator.js
+++ client/AppNavigator.js
@@ -4,22 +4,31 @@ import { NativeRouter, Route, Switch } from 'react-router-native';
 import Splash from './src/components/splash/Splash';
 import Home from './src/views/Home';
 import Login from './src/components/auth';
+import Subtopics from './src/components/subtopics/Subtopics';
+import Discussions from './src/components/discussions/Discussions';
+import PrivateRoute from './src/components/PrivateRoute';
 import NativeHeader from './src/components/navbar/NativeHeader';
 import NativeFooter from './src/components/navbar/NativeFooter';
 
-const AppNavigator = (props) => {
-    return (
-        <NativeRouter>
-            <Container>
-                <Switch>
-                    <Route path="/home" component={Home} />
-                    <Route exact path="/" component={Splash} />
-                    <Route path="/login" component={Login} />
-                </Switch>
-            </Container>
-        </NativeRouter>
-    )
-}
+const AppNavigator = props => {
+  return (
+    <NativeRouter>
+      <Container>
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route exact path="/" component={Splash} />
+          <Route path="/login" component={Login} />
 
+          <PrivateRoute strict path="/subtopics" component={Subtopics} />
+          <PrivateRoute
+            strict
+            path="/:subtopic_id/discussions"
+            component={Discussions}
+          />
+        </Switch>
+      </Container>
+    </NativeRouter>
+  );
+};
 
 export default AppNavigator;
