6c010d09867bca34f75a1e1a34c89f01c8108a67
mparedes003 <onnabugeishadev@gmail.com>
2019-06-15 12:51:20 -0700

    Added buttons on NativeFooter to test routes setup

--- client/src/components/navbar/NativeFooter.js
+++ client/src/components/navbar/NativeFooter.js
@@ -3,30 +3,43 @@ import { Footer, FooterTab, Button, Icon, Text } from 'native-base';
 import { Link } from 'react-router-native';
 
 const NativeFooter = () => {
-    return (
+  return (
+    <Footer>
+      <FooterTab>
+        <Button vertical>
+          <Link to="/">
+            <Icon name="home" />
+          </Link>
+        </Button>
 
-        <Footer>
-        <FooterTab>
+        <Button vertical>
+          <Icon name="brush" />
+        </Button>
 
-            <Button vertical>
-                <Link to="/">
-                    <Icon name="home" />
-                </Link>
-            </Button>
+        <Button vertical>
+          <Link to="/subtopics">
+            <Icon name="paper" />
+          </Link>
+        </Button>
 
-            <Button vertical>
-                <Icon name="brush" />
-            </Button>
+        <Button vertical>
+          <Link to="/:subtopic_id/discussions">
+            <Icon name="chatbubbles" />
+          </Link>
+        </Button>
 
-            <Button vertical active>
-                <Link to='/login'>
-                    <Icon active name="key" style={{ transform: [{ rotate: '-90deg' }] }} />
-                </Link>
-            </Button>
-
-        </FooterTab>
-     </Footer>
-    )
-}
+        <Button vertical active>
+          <Link to="/login">
+            <Icon
+              active
+              name="key"
+              style={{ transform: [{ rotate: '-90deg' }] }}
+            />
+          </Link>
+        </Button>
+      </FooterTab>
+    </Footer>
+  );
+};
 
 export default NativeFooter;
