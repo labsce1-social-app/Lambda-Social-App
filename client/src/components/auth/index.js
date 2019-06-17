12177bb086d3de98b26d770fdde4a4c2da2c7035
mparedes003 <onnabugeishadev@gmail.com>
2019-06-17 09:53:52 -0700

    Comment out Auth Login code in components. Transferring it to Requests.js

--- client/src/components/auth/index.js
+++ client/src/components/auth/index.js
@@ -1,162 +1,162 @@
-import React, { useContext, useEffect } from 'react';
+// import React, { useContext, useEffect } from 'react';
 
-import { Store } from '../../context/index';
+// import { Store } from '../../context/index';
 
-import {
-  StyleSheet,
-  View,
-  Text,
-  Alert,
-  StatusBar,
-  AsyncStorage
-} from 'react-native';
+// import {
+//   StyleSheet,
+//   View,
+//   Text,
+//   Alert,
+//   StatusBar,
+//   AsyncStorage
+// } from 'react-native';
 
-import {
-  Header,
-  Container,
-  Right,
-  Button,
-  Left,
-  Body,
-  Spinner
-} from 'native-base';
+// import {
+//   Header,
+//   Container,
+//   Right,
+//   Button,
+//   Left,
+//   Body,
+//   Spinner
+// } from 'native-base';
 
-import jwtDecode from 'jwt-decode';
+// import jwtDecode from 'jwt-decode';
 
-import { AUTH0_CLIENT, AUTH0_DOMAIN, BASE_URL, LOCAL } from 'react-native-dotenv';
+// import { AUTH0_CLIENT, AUTH0_DOMAIN, BASE_URL, LOCAL } from 'react-native-dotenv';
 
-const auth0ClientId = AUTH0_CLIENT;
-const auth0Domain = AUTH0_DOMAIN;
+// const auth0ClientId = AUTH0_CLIENT;
+// const auth0Domain = AUTH0_DOMAIN;
 
-import Auth0 from 'react-native-auth0';
-const auth0 = new Auth0({ domain: auth0Domain, clientId: auth0ClientId });
+// import Auth0 from 'react-native-auth0';
+// const auth0 = new Auth0({ domain: auth0Domain, clientId: auth0ClientId });
 
-const Login = props => {
-  const { state, dispatch } = useContext(Store);
+// const Login = props => {
+//   const { state, dispatch } = useContext(Store);
 
-  const handleAuth = () => {
-    auth0.webAuth
-      .authorize({
-        scope: 'openid profile email offline_access',
-        audience: 'https://lambdasocial.auth0.com/userinfo',
-        prompt: 'login'
-      })
-      .then(credentials => {
-        console.log('creds', credentials);
-        const { accessToken, idToken } = credentials;
+//   const handleAuth = () => {
+//     auth0.webAuth
+//       .authorize({
+//         scope: 'openid profile email offline_access',
+//         audience: 'https://lambdasocial.auth0.com/userinfo',
+//         prompt: 'login'
+//       })
+//       .then(credentials => {
+//         console.log('creds', credentials);
+//         const { accessToken, idToken } = credentials;
 
-        getUser(accessToken); // send access_token
+//         getUser(accessToken); // send access_token
 
-        dispatch({ type: 'LOGIN', payload: accessToken });
-      })
-      .catch(error => console.log('error in login', error));
-  };
+//         dispatch({ type: 'LOGIN', payload: accessToken });
+//       })
+//       .catch(error => console.log('error in login', error));
+//   };
 
-  // Call auth0 for user info
-  getUser = async token => {
-    await AsyncStorage.setItem('accessToken', token);
+//   // Call auth0 for user info
+//   getUser = async token => {
+//     await AsyncStorage.setItem('accessToken', token);
 
-    auth0.auth
-      .userInfo({ token: token })
-      .then(userInfo => {
-        console.log('userInfo func', userInfo);
+//     auth0.auth
+//       .userInfo({ token: token })
+//       .then(userInfo => {
+//         console.log('userInfo func', userInfo);
 
-        dispatch({ type: 'USER_INFO', payload: userInfo });
+//         dispatch({ type: 'USER_INFO', payload: userInfo });
 
-        makeUser(token, userInfo);
-      })
-      .catch(console.error);
-  };
+//         makeUser(token, userInfo);
+//       })
+//       .catch(console.error);
+//   };
 
-  // save that access_token similar to localstorage
-  const makeUser = async (token, info) => {
-    const body = JSON.stringify({
-      username: info.nickname,
-      user_id: info.sub,
-      email: info.email,
-      avatar: info.picture
-    }); // send  nickname as a 'username'
+//   // save that access_token similar to localstorage
+//   const makeUser = async (token, info) => {
+//     const body = JSON.stringify({
+//       username: info.nickname,
+//       user_id: info.sub,
+//       email: info.email,
+//       avatar: info.picture
+//     }); // send  nickname as a 'username'
 
-    await fetch(`${BASE_URL}/users`, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body
-    }).catch(error => {
-      console.log('error in sending user', error);
-    });
-  };
+//     await fetch(`${BASE_URL}/users`, {
+//       method: 'POST',
+//       headers: {
+//         Accept: 'application/json',
+//         'Content-Type': 'application/json'
+//       },
+//       body
+//     }).catch(error => {
+//       console.log('error in sending user', error);
+//     });
+//   };
 
-  const handleLogout = async () => {
-    AsyncStorage.removeItem('accessToken');
+//   const handleLogout = async () => {
+//     AsyncStorage.removeItem('accessToken');
 
-    dispatch({ type: 'LOGOUT', payload: 'You logged out' });
-  };
+//     dispatch({ type: 'LOGOUT', payload: 'You logged out' });
+//   };
 
-  return (
-    <Container>
-      <Header transparent>
-        {state.access ? (
-          <View>
-            <Right>
-              <Button
-                style={styles.AuthButton}
-                title="logout"
-                onPress={() => handleLogout()}
-              >
-                <Text style={styles.buText}>Logout</Text>
-              </Button>
-            </Right>
-            <Body>
-              {state.profile.nickname ? (
-                <Text>{state.profile.nickname}</Text>
-              ) : (
-                  <Text>{state.profile.name}</Text>
-                )}
-            </Body>
-          </View>
-        ) : (
-            <Right>
-              <Button
-                style={styles.AuthButton}
-                title="login"
-                onPress={() => handleAuth()}
-              >
-                <Text style={styles.buText}>Login</Text>
-              </Button>
-            </Right>
-          )}
-      </Header>
+//   return (
+//     <Container>
+//       <Header transparent>
+//         {state.access ? (
+//           <View>
+//             <Right>
+//               <Button
+//                 style={styles.AuthButton}
+//                 title="logout"
+//                 onPress={() => handleLogout()}
+//               >
+//                 <Text style={styles.buText}>Logout</Text>
+//               </Button>
+//             </Right>
+//             <Body>
+//               {state.profile.nickname ? (
+//                 <Text>{state.profile.nickname}</Text>
+//               ) : (
+//                   <Text>{state.profile.name}</Text>
+//                 )}
+//             </Body>
+//           </View>
+//         ) : (
+//             <Right>
+//               <Button
+//                 style={styles.AuthButton}
+//                 title="login"
+//                 onPress={() => handleAuth()}
+//               >
+//                 <Text style={styles.buText}>Login</Text>
+//               </Button>
+//             </Right>
+//           )}
+//       </Header>
 
-      {/* <StatusBar backgroundColor="#ffffff" /> */}
+//       {/* <StatusBar backgroundColor="#ffffff" /> */}
 
-      {/* {state.profile ? <Text>{state.profile.nickname}</Text> : <Text>foo</Text>} */}
-    </Container>
-  );
-};
+//       {/* {state.profile ? <Text>{state.profile.nickname}</Text> : <Text>foo</Text>} */}
+//     </Container>
+//   );
+// };
 
-export default Login;
+// export default Login;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  title: {
-    fontSize: 20,
-    textAlign: 'center',
-    marginTop: 40
-  },
-  AuthButton: {
-    backgroundColor: '#990000'
-  },
-  buText: {
-    color: '#ffffff',
-    paddingRight: 5,
-    paddingLeft: 5
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center'
+//   },
+//   title: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     marginTop: 40
+//   },
+//   AuthButton: {
+//     backgroundColor: '#990000'
+//   },
+//   buText: {
+//     color: '#ffffff',
+//     paddingRight: 5,
+//     paddingLeft: 5
+//   }
+// });
