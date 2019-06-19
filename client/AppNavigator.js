bad217877756cf0a575aecdd07852358d6a24c79
Clantigua2 <clantigua954@gmail.com>
2019-06-18 20:25:34 -0400

    fixed bug in auth issue#64

--- client/AppNavigator.js
+++ client/AppNavigator.js
@@ -4,12 +4,10 @@ import { NativeRouter, Route, Switch } from 'react-router-native';
 import Splash from './src/components/splash/Splash';
 import Home from './src/views/Home';
 import Login from './src/components/auth';
-import PrivateRoute from './src/components/PrivateRoute';
 import DiscussionsPage from './src/views/DiscussionsPage';
 import PostsPage from './src/views/PostPage';
 import SubtopicsPage from './src/views/SubtopicsPage';
 import Stack from 'react-router-native-stack';
-import PrivateRoute from './src/components/auth';
 
 
 // this component serves as a routing page, it will render everything based on the current url so it will be used to navigate the site.
@@ -20,19 +18,10 @@ const AppNavigator = (props) => {
             <Container>
                 <Stack>
                     <Route exact path="/home" component={Home} />
-
                     <Route exact path="/" component={Splash} />
-
-
                     <Route exact path="/login" component={Login} />
-
-
                     <Route exact path="/subtopics" component={SubtopicsPage} />
-
-
                     <Route exact path="/discussions/:id" component={DiscussionsPage} />
-
-
                     <Route exact path="/post/:id" component={PostsPage} />
                 </Stack>
             </Container>
