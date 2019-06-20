0af0af64bf6f78a8a1d9bfbf56cfe7092298feff
Clantigua2 <clantigua954@gmail.com>
2019-06-20 18:50:57 -0400

    added dotenv to server root file

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 // install heroku global `npm install -g heroku`
 // login heroku `heroku login`
 //From the top level of repo use command `git subtree push --prefix server heroku-backend master`
-
+require('dotenv').config();
 const server = require('./api/server.js');
+
 const port = process.env.PORT || 3000;
 
 // start the server
