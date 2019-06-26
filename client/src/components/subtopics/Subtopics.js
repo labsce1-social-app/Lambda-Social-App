84b26610235e716fa4c7351db4ece5249bc17321
Luisdh <luisd824@gmail.com>
2019-06-26 17:14:48 -0400

    remove history

--- client/src/components/subtopics/Subtopics.js
+++ client/src/components/subtopics/Subtopics.js
@@ -6,34 +6,36 @@ import { Text } from 'native-base';
 import { BASE_URL, LOCAL } from 'react-native-dotenv';
 import { withRouter } from 'react-router-native';
 
-const Subtopics = ({ history }) => {
-    const { state } = useContext(Store);
+const Subtopics = props => {
+  const { state } = useContext(Store);
 
-    return (
-        state.subtopics_loading === true ? <Text>Loading...</Text> : (
-            <FlatList
-                data={state.subtopics}
-                renderItem={({ item }) => {
-                    return (
-                        <Suspense fallback={<Text>Loading...</Text>}>
-                            <Subtopic
-                                changeLink={() => history.push({
-                                    pathname: `/discussions/${item.id}`,
-                                    id: item.id
-                                })}
-                                id={item.id}
-                                title={item.title.split(' ').join('-')}
-                                name={item.username}
-                                date={item.date !== item.updated ? item.updated : item.date}
-                            />
-                        </Suspense>
-                    )
-                }}
-                keyExtractor={(item) => `${item.id}`}
-                refreshing={state.subtopics_loading}
+  return state.subtopics_loading === true ? (
+    <Text>Loading...</Text>
+  ) : (
+    <FlatList
+      data={state.subtopics}
+      renderItem={({ item }) => {
+        return (
+          <Suspense fallback={<Text>Loading...</Text>}>
+            <Subtopic
+              changeLink={() =>
+                history.push({
+                  pathname: `/discussions/${item.id}`,
+                  id: item.id
+                })
+              }
+              id={item.id}
+              title={item.title.split(' ').join('-')}
+              name={item.username}
+              date={item.date !== item.updated ? item.updated : item.date}
             />
-        )
-    )
+          </Suspense>
+        );
+      }}
+      keyExtractor={item => `${item.id}`}
+      refreshing={state.subtopics_loading}
+    />
+  );
 };
 
-export default withRouter(Subtopics);
+export default Subtopics;
