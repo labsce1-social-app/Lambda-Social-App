02786fbb6982344faab6c73b274c44e43049a84e
Luisdh <luisd824@gmail.com>
2019-06-26 15:49:22 -0400

    change top discussions

--- client/src/components/discussions/TopDiscussions.js
+++ client/src/components/discussions/TopDiscussions.js
@@ -1,38 +1,46 @@
-import React, { useContext, lazy, Suspense } from 'react';
+import React, { useContext, lazy, Suspense, useEffect } from 'react';
 import { withRouter } from 'react-router-native';
 import { FlatList } from 'react-native-gesture-handler';
 import { Store } from '../../context';
 const Discussion = lazy(() => import('./Discussion'));
 import { Text } from 'native-base';
 
+import { getDiscussions, isAuthed } from '../../utils/Requests';
+
 const TopDiscussions = ({ history }) => {
-    const { state } = useContext(Store);
-    return (
-        state.top_discussions_loading === true ? <Text>Loading...</Text> : (
-            <FlatList
-                data={state.top_discussions}
-                renderItem={({ item }) => {
-                    return (
-                        <Suspense fallback={<Text>Loading...</Text>}>
-                            <Discussion
-                                changeLink={() => history.push(`/post/${item.id}`)}
-                                image={item.image}
-                                title={item.title.split(' ').join('-')}
-                                discussion={item.content}
-                                name={item.username}
-                                date={item.created_at}
-                                comment={item.comments}
-                                upvotes={item.upvotes}
-                            />
-                        </Suspense>
-                    )
-                }}
-                keyExtractor={(item, index) => `${index}-${item.id}`}
-                refreshing={state.top_discussions_loading}
+  const { state, dispatch } = useContext(Store);
+
+  useEffect(() => {
+    getDiscussions(state.sortBy, dispatch);
+  }, []);
+
+  //   console.log('logging inside top discussions', state.top_discussions);
+
+  return state.top_discussions_loading === true ? (
+    <Text>Loading...</Text>
+  ) : (
+    <FlatList
+      data={state.top_discussions}
+      renderItem={({ item }) => {
+        return (
+          <Suspense fallback={<Text>Loading...</Text>}>
+            <Discussion
+              // changeLink={() => history.push(`/post/${item.id}`)}
+              image={item.image}
+              title={item.title.split(' ').join('-')}
+              discussion={item.content}
+              name={item.username}
+              date={item.created_at}
+              comment={item.comments}
+              upvotes={item.upvotes}
             />
-        )
-    )
-}
+          </Suspense>
+        );
+      }}
+      keyExtractor={(item, index) => `${index}-${item.id}`}
+      refreshing={state.top_discussions_loading}
+    />
+  );
+};
 
-
-export default withRouter(TopDiscussions)
+export default withRouter(TopDiscussions);
