dc37a6f6ad3b77d469fa200fc507dee75415d72c
Luisdh <luisd824@gmail.com>
2019-06-26 15:45:17 -0400

    init react navigation bottom nav

--- client/AppNavigator.js
+++ client/AppNavigator.js
@@ -10,20 +10,53 @@ import PostsPage from './src/views/PostPage';
 import SubtopicsPage from './src/views/SubtopicsPage';
 import SubtopicForm from './src/components/subtopics/SubtopicForm';
 
-// this component serves as a routing page, it will render everything based on the current url so it will be used to navigate the site.
-const AppNavigator = props => {
-  return (
-    <BackButton>
-      <Stack>
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/" component={Splash} />
-        <Route exact path="/subtopics" component={SubtopicsPage} />
-        <Route exact path="/discussions/:id" component={DiscussionsPage} />
-        <Route exact path="/post/:id" component={PostsPage} />
-        <Route exact path="/createsubtopic" component={SubtopicForm} />
-      </Stack>
-    </BackButton>
-  );
-};
+import TopDiscussions from './src/components/discussions/TopDiscussions';
 
-export default withRouter(AppNavigator);
+import {
+  createDrawerNavigator,
+  createAppContainer,
+  createStackNavigator
+} from 'react-navigation';
+import { createBottomTabNavigator, BottomTabBar } from 'react-navigation-tabs';
+import { Icon } from 'native-base';
+
+// this component serves as a routing page, it will render everything based on the current url so it will be used to navigate the site.
+const AppNavigator = createBottomTabNavigator(
+  {
+    // Splash: Splash,
+
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        tabBarIcon: <Icon name="home" />
+      }
+    }
+  },
+  {
+    // initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f4511e'
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold'
+      }
+    }
+  }
+);
+
+export default createAppContainer(AppNavigator);
+
+// return (
+//   <BackButton>
+//     <Stack>
+//       <Route exact path="/home" component={Home} />
+//       <Route exact path="/" component={Splash} />
+//       <Route exact path="/subtopics" component={SubtopicsPage} />
+//       <Route exact path="/discussions/:id" component={DiscussionsPage} />
+//       <Route exact path="/post/:id" component={PostsPage} />
+//       <Route exact path="/createsubtopic" component={SubtopicForm} />
+//     </Stack>
+//   </BackButton>
+// );
