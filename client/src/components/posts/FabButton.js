cafc8a66f3c9f9de299429f92cfe7a7c3fb463b7
Clantigua2 <clantigua954@gmail.com>
2019-07-02 00:19:58 -0400

    styled Fab buttons

--- client/src/components/posts/FabButton.js
+++ client/src/components/posts/FabButton.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Button, Icon, Fab } from 'native-base';
+import { StyleSheet } from 'react-native';
 
 const FabButton = (props) => {
 
@@ -10,17 +11,17 @@ const FabButton = (props) => {
             active={active}
             direction="up"
             containerStyle={{}}
-            style={{ backgroundColor: '#5067FF' }}
+            style={[{ backgroundColor: '#5067FF' }, styles.shadow]}
             position="bottomRight"
             onPress={() => setActive(!active)}>
-            <Icon name="share" />
-            <Button style={{ backgroundColor: '#34A34F' }}>
-                <Icon name="logo-whatsapp" />
+            <Icon name="arrow-up" />
+            <Button style={[{ backgroundColor: '#34A34F' }, styles.shadow]}>
+                <Icon name="md-code-working" />
             </Button>
-            <Button style={{ backgroundColor: '#3B5998' }}>
-                <Icon name="logo-facebook" />
+            <Button style={[{ backgroundColor: '#3B5998' }, styles.shadow]}>
+                <Icon name="md-star-outline" />
             </Button>
-            <Button disabled style={{ backgroundColor: '#DD5144' }}>
+            <Button disabled style={[{ backgroundColor: '#DD5144' }, styles.shadow]}>
                 <Icon name="mail" />
             </Button>
         </Fab>
@@ -28,3 +29,15 @@ const FabButton = (props) => {
 }
 
 export default FabButton;
+
+const styles = StyleSheet.create({
+    shadow: {
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 3
+        },
+        shadowRadius: 5,
+        shadowOpacity: 1.0
+    }
+})
