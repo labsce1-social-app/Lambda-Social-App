9b05e8d76c103db15d81283f9b049217667bf5d3
Luisdh <luisd824@gmail.com>
2019-07-08 19:28:00 -0400

    restyle from cards

--- client/src/components/discussions/CreateDiscussion.js
+++ client/src/components/discussions/CreateDiscussion.js
@@ -12,9 +12,10 @@ import {
   CardItem,
   Text,
   Toast,
-  Spinner
+  Spinner,
+  Container
 } from 'native-base';
-import { Image } from 'react-native';
+import { Image, TextInput } from 'react-native';
 import { addDiscussion, uploadImage } from '../../utils/Requests';
 import { isEmpty } from '../../utils/utility';
 import { Store } from '../../context';
@@ -23,7 +24,9 @@ const CreateDiscussion = props => {
   const { state, dispatch } = useContext(Store);
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+
   const [image, setImage] = useState(null);
+
   const subId = props.navigation.getParam('subId');
 
   console.log('subtopic id for discussion', subId);
@@ -68,78 +71,95 @@ const CreateDiscussion = props => {
   };
 
   return (
-    <Card
+    <Container
       style={{
-        flex: 1,
-        alignContent: 'center',
-        justifyContent: 'center',
+        // flex: 1,
+        // alignContent: 'center',
+        // justifyContent: 'center',
         height: '100%'
       }}
     >
-      <CardItem>
-        <Content>
-          <Form
+      {/* <CardItem> */}
+      <Content padder>
+        <Form
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            // justifyContent: 'space-evenly',
+            width: '100%',
+            height: '100%'
+          }}
+        >
+          {/* <Text style={{ fontSize: 24 }}>Let's start a discussion.. </Text> */}
+          {/* <Label>Title</Label> */}
+          {/* <Item> */}
+          <TextInput
+            style={{ width: '100%' }}
+            placeholder="title"
+            onChangeText={e => setTitle(e)}
+          />
+          {/* </Item> */}
+          {/* <Label>Tell us what your post is about...</Label> */}
+          {/* <Item> */}
+          <View style={{ padding: 5, flex: 1 }}>
+            <TextInput
+              style={{
+                // width: '100%',
+                height: 150,
+                justifyContent: 'flex-start'
+              }}
+              placeholderTextColor="grey"
+              numberOLines={10}
+              multiline={true}
+              placeholder="post is about"
+              onChangeText={e => setContent(e)}
+            />
+          </View>
+          {/* </Item> */}
+
+          <View
             style={{
               flex: 1,
-              alignItems: 'center',
-              justifyContent: 'space-evenly',
-              width: '100%',
-              height: 400
+              flexDirection: 'row',
+              justifyContent: 'space-around',
+              marginTop: 40,
+              width: '80%'
             }}
           >
-            <Text style={{ fontSize: 24 }}>Let's start a discussion.. </Text>
-            <Item floatingLabel>
-              <Label>Title</Label>
-              <Input onChangeText={e => setTitle(e)} />
-            </Item>
-            <Item floatingLabel>
-              <Label>Tell us what your post is about...</Label>
-              <Input onChangeText={e => setContent(e)} />
-            </Item>
-
-            <View
-              style={{
-                flex: 1,
-                flexDirection: 'row',
-                justifyContent: 'space-around',
-                marginTop: 40,
-                width: '80%'
-              }}
-            >
-              <Button bordered onPress={() => uploadImage(dispatch)}>
-                <Text>Upload an image</Text>
+            <Button bordered onPress={() => uploadImage(dispatch)}>
+              <Text>Upload an image</Text>
+            </Button>
+            {state.newImage_loading === true ? (
+              <Button bordered light>
+                <Text>Done</Text>
               </Button>
-              {state.newImage_loading === true ? (
-                <Button bordered light>
-                  <Text>Done</Text>
-                </Button>
-              ) : (
-                <Button bordered success onPress={() => submitHandler()}>
-                  <Text>Done</Text>
-                </Button>
-              )}
-            </View>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginTop: 40
-              }}
-            >
-              {image !== null ? (
-                <Image
-                  source={{ uri: image }}
-                  style={{ width: 200, height: 200 }}
-                />
-              ) : state.newImage_loading === true ? (
-                <Spinner />
-              ) : null}
-            </View>
-          </Form>
-        </Content>
-      </CardItem>
-    </Card>
+            ) : (
+              <Button bordered success onPress={() => submitHandler()}>
+                <Text>Done</Text>
+              </Button>
+            )}
+          </View>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 40
+            }}
+          >
+            {image !== null ? (
+              <Image
+                source={{ uri: image }}
+                style={{ width: 200, height: 200 }}
+              />
+            ) : state.newImage_loading === true ? (
+              <Spinner />
+            ) : null}
+          </View>
+        </Form>
+      </Content>
+      {/* </CardItem> */}
+    </Container>
   );
 };
 
