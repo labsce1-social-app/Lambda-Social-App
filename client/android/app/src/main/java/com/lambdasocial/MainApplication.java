104d17197ef9436a076a77eb7cd00408f8911a93
Clantigua2 <clantigua954@gmail.com>
2019-07-11 15:31:54 -0400

    initialized basic fab component rework

--- client/android/app/src/main/java/com/lambdasocial/MainApplication.java
+++ client/android/app/src/main/java/com/lambdasocial/MainApplication.java
@@ -3,6 +3,7 @@ package com.lambdasocial;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.amazonaws.RNAWSCognitoPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new SvgPackage(),
             new RNAWSCognitoPackage(),
             new ImagePickerPackage(),
