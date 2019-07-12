4bf9a07de7f5ae1b5606131f0a5f5209d8a37494
Luisdh <luisd824@gmail.com>
2019-07-12 16:05:43 -0400

    update query join subtopic table return sub title

--- server/api/helpers/userSubsHelper.js
+++ server/api/helpers/userSubsHelper.js
@@ -3,8 +3,11 @@ const db = require('../../data/dbconfig.js');
 const getUsersFavSubtopics = id => {
   return db
     .raw(
-      `SELECT subtopic_users.user_id, subtopic_users.subtopic_id FROM subtopic_users 
-WHERE subtopic_users.user_id = '${id}'`
+      `SELECT subtopic_users.user_id as user_id, subtopic_users.subtopic_id as subtopic_id, subtopic.title as subtopic_title, subtopic.creater_id
+      FROM subtopic_users
+      inner JOIN subtopic
+      on subtopic.id = subtopic_users.subtopic_id
+      WHERE subtopic_users.user_id = '${id}'`
     )
     .then(res => res.rows);
 };
