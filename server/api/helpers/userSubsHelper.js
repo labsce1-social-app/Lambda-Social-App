7adb2bef33d34f2a944f67e149dea99b6008c923
Luisdh <luisd824@gmail.com>
2019-07-12 20:36:31 -0400

    select id in query to get favorite subtopic

--- server/api/helpers/userSubsHelper.js
+++ server/api/helpers/userSubsHelper.js
@@ -3,7 +3,11 @@ const db = require('../../data/dbconfig.js');
 const getUsersFavSubtopics = id => {
   return db
     .raw(
-      `SELECT subtopic_users.user_id as user_id, subtopic_users.subtopic_id as subtopic_id, subtopic.title as subtopic_title, subtopic.creater_id
+      `SELECT subtopic_users.id, 
+      subtopic_users.user_id as user_id, 
+      subtopic_users.subtopic_id as subtopic_id, 
+      subtopic.title as subtopic_title, 
+      subtopic.creater_id
       FROM subtopic_users
       inner JOIN subtopic
       on subtopic.id = subtopic_users.subtopic_id
@@ -15,7 +19,7 @@ const getUsersFavSubtopics = id => {
 const addFavoriteSubtopicToUser = body => {
   console.log('WE ARE INSERTING: ', body);
   return db('subtopic_users')
-    .insert(body, ['subtopic_id', 'user_id'])
+    .insert(body, ['id', 'subtopic_id', 'user_id'])
     .then(row => row);
 };
 
