7e03e2b6468694beaf067eaaf0bbc28d33aa889b
Luisdh <luisd824@gmail.com>
2019-07-12 20:37:50 -0400

    favoriting subtopic returns updated array of favorites

--- server/api/routers/subtopicUsersRouter.js
+++ server/api/routers/subtopicUsersRouter.js
@@ -25,7 +25,9 @@ router.post('/favorite', (req, res) => {
   addFavoriteSubtopicToUser(req.body)
     .then(ret => {
       console.log('SUCCESS ADDING: ', ret);
-      res.status(200).json(ret[0]);
+      getUsersFavSubtopics(ret[0].user_id).then(favSubs => {
+        res.status(200).json(favSubs);
+      });
     })
     .catch(err => {
       console.log('COULDNT INSERT ', err);
