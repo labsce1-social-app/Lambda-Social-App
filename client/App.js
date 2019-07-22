4583db4340acd63f3003964b3cda8285863ca922
Clantigua2 <clantigua954@gmail.com>
2019-07-22 17:39:33 -0400

    setup initial socket stuff

--- client/App.js
+++ client/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppNavigator from './AppNavigator';
 import { Root, StyleProvider } from 'native-base';
 import { StoreProvider } from './src/context/';
 import getTheme from './native-base-theme/components';
 import { darkTheme } from './native-base-theme/variables/darkTheme';
 import material from './native-base-theme/variables/material';
-// const instructions = Platform.select({
-//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-//   android:
-//     'Double tap R on your keyboard to reload,\n' +
-//     'Shake or press menu button for dev menu',
-// });
+import io from 'socket.io-client';
+import { LOCAL, IP } from 'react-native-dotenv';
 
 // this is where the entire app gets exported from, the context store provider is wrapped around here to give everything access to the store
-export default (App = () => (
-  <StyleProvider style={getTheme(darkTheme)}>
-    <StoreProvider>
-      <Root>
-        <AppNavigator />
-      </Root>
-    </StoreProvider>
-  </StyleProvider>
-));
+export default (App = () => {
+  useEffect(() => {
+    //needs local ip address
+    // for windows use ipconfig in terminal
+    // for mac use ifconfig in terminal
+    // const socket = io(`http://192.168.68.128:3000`);
+    const socket = io(LOCAL);
+  }, [])
+
+  return (
+    <StyleProvider style={getTheme(darkTheme)}>
+      <StoreProvider>
+        <Root>
+          <AppNavigator />
+        </Root>
+      </StoreProvider>
+    </StyleProvider>
+  )
+});
