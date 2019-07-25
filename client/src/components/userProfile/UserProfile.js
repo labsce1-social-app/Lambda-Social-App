f6f64eec4c9ff7ef459df758559aba9bc0e1268f
Clantigua2 <clantigua954@gmail.com>
2019-07-24 21:14:04 -0400

    setup base userprofile component

---
+++ client/src/components/userProfile/UserProfile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const UserProfile = () => {
+    return (
+        <View>
+            <Text>Hello world</Text>
+        </View>
+    )
+}
+
+export default UserProfile;
