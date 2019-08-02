8511caf211d4e692c260e2ee201d9a145c691289
Luisdh <luisd824@gmail.com>
2019-08-02 18:07:35 -0400

    get permission and device token for fcm

--- client/App.js
+++ client/App.js
@@ -7,16 +7,52 @@ import { darkTheme } from './native-base-theme/variables/darkTheme';
 import material from './native-base-theme/variables/material';
 import io from 'socket.io-client';
 // import { LOCAL, IP } from 'react-native-dotenv';
+import firebase from 'react-native-firebase';
+import AsyncStorage from '@react-native-community/async-storage';
 
 // this is where the entire app gets exported from, the context store provider is wrapped around here to give everything access to the store
 export default (App = () => {
-  // useEffect(() => {
-  //   //needs local ip address
-  //   // for windows use ipconfig in terminal
-  //   // for mac use ifconfig in terminal
-  //   // const socket = io(`http://192.168.68.128:3000`);
-  //   const socket = io(LOCAL);
-  // }, [])
+  useEffect(() => {
+    checkPermission();
+    // console.log(firebase);
+  }, []);
+
+  const checkPermission = async () => {
+    try {
+      const enabled = await firebase.messaging().hasPermission();
+      if (enabled) {
+        getToken();
+      } else {
+        requestPermission();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getToken = async () => {
+    let fcmToken = await AsyncStorage.getItem('fcmToken');
+    if (!fcmToken) {
+      fcmToken = await firebase.messaging().getToken();
+      console.log('DIDNT GET TOKEN', fcmToken);
+      if (fcmToken) {
+        // user has a device token
+        console.log('GOT TOKEN', fcmToken);
+        await AsyncStorage.setItem('fcmToken', fcmToken);
+      }
+    }
+  };
+
+  const requestPermission = async () => {
+    try {
+      await firebase.messaging().requestPermission();
+      // User has authorised
+      getToken();
+    } catch (error) {
+      // User has rejected permissions
+      console.log('permission rejected');
+    }
+  };
 
   return (
     <StyleProvider style={getTheme(darkTheme)}>
